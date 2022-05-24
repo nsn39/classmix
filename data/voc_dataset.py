69603693e0f1c2838107b59d9c87305827ca4d64
nsn39 <unseenguy42@gmail.com>
2022-05-24 20:31:40 +0545

    edit 1

--- data/voc_dataset.py
+++ data/voc_dataset.py
@@ -27,8 +27,8 @@ class VOCDataSet(data.Dataset):
         self.files = []
         # for split in ["train", "trainval", "val"]:
         for name in self.img_ids:
-            img_file = osp.join(self.root, "JPEGImages/%s.jpg" % name)
-            label_file = osp.join(self.root, "SegmentationClassAug/%s.png" % name)
+            img_file = osp.join(self.root, "VOCdevkit", "VOC2012",  "JPEGImages/%s.jpg" % name)
+            label_file = osp.join(self.root, "VOCdevkit", "VOC2012", "SegmentationClassAug/%s.png" % name)
             self.files.append({
                 "img": img_file,
                 "label": label_file,
